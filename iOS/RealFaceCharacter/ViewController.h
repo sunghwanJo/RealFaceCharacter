34430d16f941e9af0867a7e9c3df32f319f0ffc5
comelf <leebyungwoo@gmail.com>
2013-08-21 17:11:57 +0900

    iOS

---
+++ iOS/RealFaceCharacter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RealFaceCharacter
+//
+//  Created by  byung-woo Lee on 13. 8. 21..
+//  Copyright (c) 2013년 Lee & Jo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOS/RealFaceCharacter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RealFaceCharacter
+//
+//  Created by  byung-woo Lee on 13. 8. 21..
+//  Copyright (c) 2013년 Lee & Jo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
